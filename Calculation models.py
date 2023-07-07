94ca18b558bf320afc1c9754330e84f17f1afe10
danrob0910 <danrob0910@highgateschool.org.uk>
2023-07-07 09:34:32 +0100

    added a calcs model

---
+++ Calculation models.py
@@ -0,0 +1,35 @@
+import math
+
+def NL2_no_mass(acceleration,force):
+    """
+
+    Parameters
+    ----------
+    acceleration
+    force
+
+    Returns
+    -------
+
+    """
+
+def blocks_on_slopes(slope_angle, mass_block, acceleration, coefficient_of_friction, friction, g):
+    mg = mass_block * g
+    reaction_force = math.cos(slope_angle) * mg
+
+
+
+    """
+    
+    Parameters
+    ----------
+    slope_angle
+    mass_block
+    acceleration
+    coefficient_of_friction
+    friction
+
+    Returns
+    -------
+
+    """
